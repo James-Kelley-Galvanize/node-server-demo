22696f06cbad1c1ac29e3d0dd3990fd78973a76f
James-Kelley-Galvanize <james.kelley@galvanize.com>
2021-06-22 14:26:25 -0600

    Add switch

--- server.js
+++ server.js
@@ -3,10 +3,18 @@ const port = 3001;
 const host = "127.0.0.1";
 
 const server = http.createServer((request, response) => {
-  console.log(request.method);
-  console.log(request.url);
+  const { method, url } = request;
 
-  response.end("This isn't your grandma's Pokemon API");
+  switch (method) {
+    case `GET`:
+      response.end("Way to GET!");
+    case `POST`:
+      response.end("POST was your request method!");
+    default:
+      response.end(
+        "This isn't your grandma's Pokemon API - and you didn't use a GET or a POST request"
+      );
+  }
 });
 
 server.listen(port, host, () => {
